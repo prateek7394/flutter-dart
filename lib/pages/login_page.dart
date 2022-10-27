80fbd83c3c0f74cc4d747ba8117e8980a685e52c
Prateek Shukla <200108043@hbtu.ac.in>
2022-10-27 19:14:53 +0530

    Stateful Widget, InkWell, AnimatedContainer & Future.delayed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +31,7 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -34,9 +42,12 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter username", labelText: "Username"),
-                    ),
+                        decoration: InputDecoration(
+                            hintText: "Enter username", labelText: "Username"),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        }),
                     TextFormField(
                       obscureText: true, // to hide the password characters
                       decoration: InputDecoration(
@@ -48,14 +59,43 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    // -> Callback
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-                  child: Text("Login", style: TextStyle(fontSize: 18))
+              InkWell(
+                onTap: () async{
+                  setState(() {
+                    changeButton = !changeButton;
+                  });
+                  await Future.delayed(Duration(seconds: 1)) ;
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width: changeButton ? 60 : 150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: changeButton
+                          ? BorderRadius.circular(50)
+                          : BorderRadius.circular(5)),
+                  child: changeButton
+                      ? Icon(Icons.done, color: Colors.white)
+                      : Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
                 ),
+              )
+              // ElevatedButton(
+              //     onPressed: () {
+              //       // -> Callback
+              //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+              //     },
+              //     style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+              //     child: Text("Login", style: TextStyle(fontSize: 18))
+              //   ),
             ],
           ),
         ));
